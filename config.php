fa08531930625b7c28afc92176865c18793006d9
mqomaru96 <50206707+mqomaru96@users.noreply.github.com>
2019-05-05 11:13:38 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "";
+//user_token wajib di isi //
+$user_token="";
+
+//user_id wajib di isi //
+$user_id="";
+
+// invite_code wajib di isi //
+$invite_code="";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
+
+
+
+?>
